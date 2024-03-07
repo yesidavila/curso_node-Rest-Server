12e9305650c6f7401b89dee79813ff17128218da
yesidavila <161858764+yesidavila@users.noreply.github.com>
2024-03-07 08:48:56 -0500

    Update conexion puerto

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server{
 
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT|| 8080;
 
         this.usuariosPath = '/api/usuarios';
         // middleawers
